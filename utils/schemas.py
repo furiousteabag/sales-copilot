cd7f746eb5cebc281f5c5e4a95fdb33647329a98
Alexander Smirnov <furiousteabag@gmail.com>
2023-10-06 23:33:23 -0700

    add schemas

---
+++ utils/schemas.py
@@ -0,0 +1,66 @@
+from enum import Enum
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+
+class ProfileType(str, Enum):
+    person = "person"
+    company = "company"
+
+
+class CurrentCompany(BaseModel):
+    name: Optional[str]
+    link: Optional[str]
+
+
+class Experience(BaseModel):
+    title: str
+    subtitle: str
+    subtitleURL: Optional[str]
+    location: Optional[str]
+    description: Optional[str]
+    duration: str
+    start_date: str
+    end_date: str
+    duration_short: str
+
+
+class Education(BaseModel):
+    title: Optional[str]
+    degree: Optional[str]
+    field: Optional[str]
+    meta: Optional[str]
+    url: str
+    start_year: Optional[str]
+    end_year: Optional[str]
+
+
+class Activity(BaseModel):
+    title: str
+    attribution: str
+    img: str
+    link: str
+
+
+class PersonProfile(BaseModel):
+    name: str
+    position: str
+    current_company: CurrentCompany
+    avatar: str
+    about: Optional[str]
+    city: str
+    educations_details: Optional[str]
+    posts: List
+    experience: List[Experience]
+    education: List[Education]
+    activities: List[Activity]
+
+
+class CompanyProfile(BaseModel):
+    url: str
+    name: str
+    slogan: str
+    about: Optional[str]
+    website: str
+    Headquarters: Optional[str]
